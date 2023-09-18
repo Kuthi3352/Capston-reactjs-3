1255092a234b6964745236cfd0460d3b29f7629a
Kuthi <anhthidola@gmail.com>
2023-09-18 16:27:19 +0700

    start

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { RouteObject } from "react-router-dom";
+import { PATH } from "../constant/config";
+export const router: RouteObject[] = [
+    {
+        path: PATH.login,
+    },
+    {
+        path: PATH.register,
+    },
+]
